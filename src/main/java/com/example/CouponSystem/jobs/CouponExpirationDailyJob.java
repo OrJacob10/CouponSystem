aca187e8f7f920ed371fe6d2a4cd285e3f504067
OrJacob10 <orjacob428@gmail.com>
2023-09-21 16:39:39 +0300

    updated daily job to Async and Scheduled annotations

--- src/main/java/com/example/CouponSystem/jobs/CouponExpirationDailyJob.java
+++ src/main/java/com/example/CouponSystem/jobs/CouponExpirationDailyJob.java
@@ -4,13 +4,14 @@ import com.example.CouponSystem.beans.CouponDTO;
 import com.example.CouponSystem.exception.AuthorizationException;
 import com.example.CouponSystem.exception.CouponException;
 import com.example.CouponSystem.service.CouponService;
-import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import java.util.List;
 
-@NoArgsConstructor
 @Component
-public class CouponExpirationDailyJob implements Runnable{
+public class CouponExpirationDailyJob{
     @Autowired
     private CouponService couponService;
 
@@ -19,20 +20,19 @@ public class CouponExpirationDailyJob implements Runnable{
        and deletes all the coupons that their end date had passed.
        The thread executes once every 24 hours
    */
-
-    @Override
-    public void run() {
-        try {
-            if (this.couponService.getAllCouponsExpired() != null) {
-                for (CouponDTO c : this.couponService.getAllCouponsExpired()) {
-                        this.couponService.deleteCoupon(c.getId());
+    @Async
+    @Scheduled(cron = "0 0 0 * * ?")
+    public void expiredCouponJob() {
+         // Run at midnight every day
+            try {
+                List<CouponDTO> expiredCoupons = couponService.getAllCouponsExpired();
+                for (CouponDTO coupon : expiredCoupons) {
+                    couponService.deleteCoupon(coupon.getId());
                 }
-            }
-            Thread.sleep(1000 * 60 * 60 * 24);
-        } catch (InterruptedException | CouponException | AuthorizationException e) {
-            if (e instanceof CouponException){
-                System.out.println(e.getMessage());
-            }
+            } catch (CouponException | AuthorizationException e) {
+                if (e instanceof CouponException){
+                    System.out.println(e.getMessage());
+                }
         }
     }
 }
